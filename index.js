7f3504162a25284be258621fdd9e7e7e11f53859
pankaj <pj9009815223@gmail.com>
2023-03-29 10:27:20 +0530

    okk

--- index.js
+++ index.js
@@ -12,5 +12,55 @@ $(document).ready(function(){
     $('.menu-btn').click(function(){
         $('#nav-menu .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
+    });
+
+
+    $('.menu li a').click(function(){
+        $('#nav-menu .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    //   typing animation script 
+       var typed = new Typed (".panku",{
+        strings:["Full-Stack Web-Developer", "Node-js Backend Devloper"],
+        typeSpeed:100,
+        backSpeed:60,
+        loop:true
+       })
+
+
+       var typed = new Typed (".panku2",{
+        strings:["Full-Stack Web-Developer", "Node-js Backend Devloper"],
+        typeSpeed:100,
+        backSpeed:60,
+        loop:true
+       });
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+         margin:20,
+         loop:true,
+         autoplayTimeOut:2000,
+         autoplayHoverPause:true,
+         responsive:{
+            0:{
+                items:1,
+                nav:false
+            },
+            600:{
+                items:2,
+                nav:false
+            },
+            1000:{
+                items:3,
+                nav:false
+            }
+         }
     })
-})
+
+
+
+
+});
+
+
