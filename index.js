106bdc65392011ef9c9ce980fbeca6ce1cf4da36
pankaj jain <pj9009815223@gmail.com>
2023-06-05 17:33:07 +0530

    okk

--- index.js
+++ index.js
@@ -48,11 +48,11 @@ $(document).ready(function(){
                 nav:false
             },
             600:{
-                items:2,
+                items:1,
                 nav:false
             },
             1000:{
-                items:3,
+                items:1,
                 nav:false
             }
          }
